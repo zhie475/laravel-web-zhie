d4ef1cffd9a122ebdb58f58d79bdf05cab769866
zhie <zhierly24ti@mahasiswa.pcr.ac.id>
2025-10-06 15:36:02 +0700

    Laravel Validation

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -27,13 +27,16 @@ class QuestionController extends Controller
      */
     public function store(Request $request)
     {
-        //dd( $request->all());
 
-        $data['nama'] = $request->nama;
-        $data['email'] = $request->email;
-        $data['pertanyaan'] = $request->pertanyaan;
+        $request->validate([
+            'nama' => 'required|max:10',
+            'email' => ['required', 'email'],
+            'pertanyaan' => 'required|max:300|min:8',
+        ], [
+            'nama.required' => 'Nama tidak boleh kosong',
+            'email.email' => 'Email tidak valid'
+        ]);
 
-        return view('home-question-respon', $data);
     }
 
     /**
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -76,20 +76,33 @@
                         <div class="card">
                             <div class="card-body">
                                 <h5 class="card-title">Form Pertanyaan</h5>
+                                @if ($errors->any())
+                                    <div class="alert alert-danger">
+                                        <ul>
+                                            @foreach ($errors->all() as $error)
+                                                <li>{{ $error }}</li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                @endif
                                 <form action="{{ route('question.store') }}" method="POST">
                                     @csrf
                                     <form action="" method="POST">
                                         <div class="mb-3">
                                             <label for="nama" class="form-label">Nama</label>
-                                            <input type="text" ... name="nama">
+                                            <input type="text" class="form-control" id="nama" name="nama"
+                                                value="{{ old('nama') }}">
                                         </div>
+
                                         <div class="mb-3">
                                             <label for="email" class="form-label">Email</label>
-                                            <input type="text" ... name="email">
+                                            <input type="text" class="form-control" id="email" name="email"
+                                                value="{{ old('email') }}">
                                         </div>
+
                                         <div class="mb-3">
                                             <label for="pertanyaan" class="form-label">Pertanyaan</label>
-                                            <textarea ... name="pertanyaan" ...></textarea>
+                                            <textarea class="form-control" id="pertanyaan" name="pertanyaan" rows="2">{{ old('pertanyaan') }}</textarea>
                                         </div>
                                         <button type="submit" class="btn btn-primary">Kirim Pertanyaan</button>
                                     </form>
